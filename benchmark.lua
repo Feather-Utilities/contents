2bb89bf1aadc4a993019be48ba58d542b0565468
Toon <85719086+Toon-arch@users.noreply.github.com>
2022-02-15 15:28:02 -0500

    Update benchmark.lua

--- benchmark.lua
+++ benchmark.lua
@@ -1,23 +1,6 @@
-local requestfunc = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
-local getasset = getsynasset or getcustomasset
 local pointScore = 0
 local missingFunctions = {}
-
 print("---- Feather Benchmark ----")
-if requestfunc then
-    print("Request : VALID")
-    pointScore = pointScore + 1
-else
-    table.insert(missingFunctions, "Request")
-    print("Request : NOT FOUND")
-end
-if getasset then
-    print("getsynasset / getcustomasset : VALID")
-    pointScore = pointScore + 1
-else
-    table.insert(missingFunctions, "getsynasset / getcustomasset")
-    print("getsynasset / getcustomasset : NOT FOUND")
-end
 if isfolder and makefolder and writefile and isfile then
     print("Filesystem API : VALID")
     pointScore = pointScore + 1
@@ -25,11 +8,10 @@ else
     table.insert(missingFunctions, "Filesystem API")
     print("Filesystem API : NOT FOUND")
 end
-
-if pointScore >= 3 then
-    print("ALL Functions Found! You can use Feather Admin without any issues.")
+if pointScore >= 1 then
+    print("All Functions Found! You can use Feather Admin without any issues.")
 else
-    print("Functions Missing! Feather Admin will not function properly on your Exploit. Total Points: " .. pointScore .. ". Total Required: 3. Missing Functions:\n")
+    print("Functions Missing! Feather Admin will not function properly on your Exploit. Total Points: " .. pointScore .. ". Total Required: 1. Missing Functions:\n")
     for i,v in pairs(missingFunctions) do
         print(v .. "\n")
     end
